479476dbfdaf48664efb6e4b7d2f56c70874dc8f
DaOranger <132038723+Daoranger@users.noreply.github.com>
2024-03-28 13:08:27 -0700

    Checklist working!

--- workwise/checklistPage.js
+++ workwise/checklistPage.js
@@ -1,12 +1,49 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const addTaskBtn = document.getElementById("add-task-btn");
 
 function addTask() {
-  if(inputBox.value === '') {// If there is no text
-    alert("you must write something!");
+  // Check if the input box is empty
+  if (inputBox.value === "") {
+    // If there is no text
+    alert("Goober write something!");
   } else {
+    // Create a new list item
     let li = document.createElement("li");
+    // Set the content of the list item to the value of the input box
     li.innerHTML = inputBox.value;
+    // Append the list item to the list container
     listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  inputBox.value = "";
+  saveData();
 }
+
+addTaskBtn.addEventListener("click", addTask);
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
--- workwise/popup.js
+++ workwise/popup.js
@@ -1,9 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var mainPageButton = document.getElementById('mainPageButton');
-  
-    mainPageButton.addEventListener('click', function() {
-      // Open main page when the button is clicked
-      chrome.tabs.create({url: 'mainPage.html'});
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var mainPageButton = document.getElementById("mainPageButton");
+
+  mainPageButton.addEventListener("click", function () {
+    // Open main page when the button is clicked
+    chrome.tabs.create({ url: "mainPage.html" });
   });
-  
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var checklistPageButton = document.getElementById("checkListPageButton");
+
+  checklistPageButton.addEventListener("click", function () {
+    // Open checklist page when the button is clicked
+    chrome.tabs.create({ url: "checklistPage.html" });
+  });
+});

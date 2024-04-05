c100f2564a6b35190ce64dab7cdd42e861f34235
DaOranger <132038723+Daoranger@users.noreply.github.com>
2024-04-05 16:22:15 -0700

    removed conflict messages trails

--- popup/popup.js
+++ popup/popup.js
@@ -1,33 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mainPageButton = document.getElementById("mainPageButton");
+  var checklistPageButton = document.getElementById("checkListPageButton");
+  var createAccountPageButton = document.getElementById(
+    "createAccountPageButton"
+  );
+  var websiteTrackingButton = document.getElementById("websiteTrackingButton");
 
   mainPageButton.addEventListener("click", function () {
     // Open main page when the button is clicked
     chrome.tabs.create({ url: "../mainPage/mainPage.html" });
   });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  var checklistPageButton = document.getElementById("checkListPageButton");
 
   checklistPageButton.addEventListener("click", function () {
     // Open checklist page when the button is clicked
     chrome.tabs.create({ url: "../checklistPage/checklistPage.html" });
   });
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-  var checklistPageButton = document.getElementById("createAccountPageButton");
-
-  checklistPageButton.addEventListener("click", function () {
-    // Open checklist page when the button is clicked
+  createAccountPageButton.addEventListener("click", function () {
+    // Open create account page when the button is clicked
     chrome.tabs.create({ url: "../signupPage/signup.html" });
   });
-});
 
-var checklistPageButton = document.getElementById("websiteTrackingButton");
-
-checklistPageButton.addEventListener("click", function () {
-  // Open checklist page when the button is clicked
-  chrome.tabs.create({ url: "../timeTrackingPage/timeTrackingPage.html" });
+  websiteTrackingButton.addEventListener("click", function () {
+    // Open time tracking page when the button is clicked
+    chrome.tabs.create({ url: "../timeTrackingPage/timeTrackingPage.html" });
+  });
 });

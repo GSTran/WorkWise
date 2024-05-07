3cdceea478b0b3578cd6ca58aed91b1b00461efe
notnuthin <116135015+notnuthin@users.noreply.github.com>
2024-05-07 00:52:12 -0700

    Time track page CSS

--- popup/popup.js
+++ popup/popup.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   var checklistPageButton = document.getElementById("checkListPageButton");
 
-  document.addEventListener("visibilitychange", function() {
-    if (document.visibilityState === 'hidden') {
-      // Popup is closed, do something
-      chrome.notifications.create(
-        {
-          type: "basic",
-          title: "jajaja",
-          message: "bruh moment",
-          iconUrl: "../img/cat.png"
-        });
-      console.log("Popup closed");
-    }
-  });
+  // document.addEventListener("visibilitychange", function() {
+  //   if (document.visibilityState === 'hidden') {
+  //     // Popup is closed, do something
+  //     chrome.notifications.create(
+  //       {
+  //         type: "basic",
+  //         title: "jajaja",
+  //         message: "bruh moment",
+  //         iconUrl: "../img/cat.png"
+  //       });
+  //     console.log("Popup closed");
+  //   }
+  // });
   
 
   window.addEventListener('beforeunload', function(event) {
